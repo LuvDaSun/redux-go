b50d0b1572578ee00794e4e9ea0e7916a7e1d5cd
Elmer Bulthuis <elmerbulthuis@gmail.com>
2019-12-03 15:12:17 +0100

    move mutex

--- example/light/middleware.go
+++ example/light/middleware.go
@@ -10,10 +10,11 @@ import (
 CreateToggleMiddleware transforms the toggle action
 */
 func CreateToggleMiddleware() redux.Middleware {
-	var mutex = &sync.Mutex{}
 
 	return func(getState redux.GetState, dispatch redux.Dispatch) redux.Chain {
 		return func(next redux.Dispatch) redux.Dispatch {
+			var mutex = &sync.Mutex{}
+
 			return func(action redux.Action) {
 				next(action)
 
